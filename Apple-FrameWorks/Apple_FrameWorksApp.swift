9118300304129c9cda340ff39b8c5e3e18fe1446
Mironisegor <mironove55@icloud.com>
2024-04-04 17:08:50 +0300

    Initial Commit

---
+++ Apple-FrameWorks/Apple_FrameWorksApp.swift
@@ -0,0 +1,17 @@
+//
+//  Apple_FrameWorksApp.swift
+//  Apple-FrameWorks
+//
+//  Created by Vladimir Fursov on 04.04.2024.
+//
+
+import SwiftUI
+
+@main
+struct Apple_FrameWorksApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
