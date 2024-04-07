854d3293aa26b9ae7663933a8e0bba6b56868875
Mironisegor <mironove55@icloud.com>
2024-04-07 19:40:37 +0300

    commit

---
+++ Apple-FrameWorks/GreetingView.swift
@@ -0,0 +1,18 @@
+//
+//  GreetingView.swift
+//  Apple-FrameWorks
+//
+//  Created by Vladimir Fursov on 07.04.2024.
+//
+
+import SwiftUI
+
+struct GreetingView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    GreetingView()
+}
